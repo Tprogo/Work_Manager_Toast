5fcfc28304f215cd3a821f76b9fa7637fbcafa92
Admin <zambuk125@gmail.com>
2024-05-04 22:20:31 +0530

    Dark Mode Added

--- app/src/main/java/com/example/workmanagerapplication/MainActivity.kt
+++ app/src/main/java/com/example/workmanagerapplication/MainActivity.kt
@@ -2,16 +2,53 @@ package com.example.workmanagerapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.work.Constraints
 import androidx.work.NetworkType
 import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
+
+
+    private var savedState: Boolean? = null
+    private lateinit var darkModeBtn: Switch
+
+    private lateinit var setThemeText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+         darkModeBtn = findViewById(R.id.switchBtn)
+         setThemeText = findViewById<TextView>(R.id.setTheme)
+
+        setThemeText.text = "Enable Dark Mode"
+
+        checkSavedThemeMode()
+
+
+
+
+        // check the theme mode
+
+
+        val pDatastore = PDatastore(this@MainActivity)
+
+
+
+
+
+
 
         //set condition for work manager to run
 
@@ -26,5 +63,58 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         WorkManager.getInstance(this).enqueue(workerRequest)
+
+
+        // set dark theme with the help of switch button
+
+        darkModeBtn.setOnCheckedChangeListener { _, isChecked ->
+
+            setThemeMode(isChecked)
+            CoroutineScope(Dispatchers.Main).launch {
+                pDatastore.saveThemeState(isChecked)
+            }
+        }
+    }
+
+    private fun setThemeMode(flag: Boolean){
+        if (flag){
+
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            setThemeText.text = "Disable Dark Mode"
+
+
+
+        } else{
+
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            setThemeText.text = "Enable Dark Mode"
+
+
+        }
+    }
+
+    private fun checkSavedThemeMode(){
+        CoroutineScope(Dispatchers.Main).launch{
+            val pDatastore = PDatastore(this@MainActivity)
+
+            pDatastore.getThemeState().collect{it ->
+
+                savedState = it
+
+                if (it != null) {
+                    darkModeBtn.isChecked = it
+                }
+
+
+
+
+
+            }
+
+
+
+
+
+        }
     }
 }
